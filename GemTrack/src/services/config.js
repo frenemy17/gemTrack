ee43656436febba95b89ac32e0d2183d751a7979
frenemy17 <raikarsiddhanth@gmail.com>
2025-12-03 18:11:02 +0530

    Delete GemTrack directory

--- GemTrack/src/services/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'http://localhost:3001/api';
-export const SHOP_NAME = 'SHREE SHARADA JEWELLERS';
-export const SHOP_ADDRESS = 'Main Road ANKOLA - 581314';
-export const SHOP_PHONE = '9448105057';
-export const SHOP_GSTIN = '29AGLPR6539C1ZZ';
